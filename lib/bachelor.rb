5da4cdedb0254128b207e4420679335e1bb4f804
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 19:29:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,5 +54,6 @@ def get_average_age_for_season(data, season)
     data[season].each do |hash|
       hash.each do |k,v|
         if k == age
-          count =+ 1
+          count += 1 
+          age += v_to.f
 end
