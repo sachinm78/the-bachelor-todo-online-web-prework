44c7c48ced98e487e2c0a2b0c320fcc633cedbcd
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 19:24:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -38,7 +38,10 @@ end
 
 def get_occupation(data, hometown)
   date.each do |season, array|
-    
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return 
 end
 
 def get_average_age_for_season(data, season)
