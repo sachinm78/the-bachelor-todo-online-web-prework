2bbceb846cbd17cad523fead00cfef17d8a4892c
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 19:16:07 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,7 +11,14 @@ end
 
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
